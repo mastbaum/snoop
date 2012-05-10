3915e7464e3b17f10a395370aeccaab4496236d3
Andy Mastbaum <amastbaum@gmail.com>
2012-05-09 22:01:54 -0500

    package with setuptools

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name = 'snoop',
+    version = '0.7',
+    description = 'sno(plus) online processing',
+    author = 'Andy Mastbaum',
+    author_email = 'mastbaum@hep.upenn.com',
+    url = 'http://github.com/mastbaum/snoop',
+    packages = ['snoop', 'snoop.core', 'snoop.processors'],
+    scripts = ['bin/snoop'],
+)
+
